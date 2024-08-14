3d3a2bcf2621c96bcb469d2221bd1c87e008e83f
Felipe Moreira <felipegandra2005@gmail.com>
2024-08-13 23:09:19 -0300

    Added saveValues and time functions

--- functions.c
+++ functions.c
@@ -1,3 +1,4 @@
+#include "functions.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -13,11 +14,13 @@ typedef struct DATA{
     int count;
 }Data;
 
-int freeArray(void **array, int n)
+
+void freeArray(void **array, int n)
 {
     if (array == NULL)
     {
-        return 1;
+        printf("The pointer was NULL\n");
+        exit(1);
     }
     for (int i = 0; i < n; i++)
     {
@@ -29,5 +32,59 @@ int freeArray(void **array, int n)
     }
     free(array);
     array = NULL;
-    return 0;
+}
+
+
+/*
+Function to create a txt file (called time.txt) to store the execution time of each
+parameter combination
+*/
+void createTimeFile(void)
+{	
+	FILE *file = fopen("time.txt", "w");
+	if (file == NULL)
+	{
+		printf("Couldn't create file time.txt\n");
+		exit(1);
+	}
+	fclose(file);
+    file = NULL;
+}
+
+
+/*
+Function to print the execution times in the time file
+*/
+void printTime(int begin, int end, char *name)
+{
+	FILE *file = fopen("time.txt", "r+");
+	if (file == NULL)
+	{
+		printf("Couldn't open file time.txt\n");
+		exit(1);
+	}
+	int time = end-begin;
+	fseek(file, 0, SEEK_END);
+	fprintf(file, "%s  %ds\n", name, time);
+	fclose(file);
+}
+
+
+/*
+Function to read and save all values of a certain parameter.It is used to save the list of 
+parameters that are going to be used.
+*/
+void saveValues(Data * parameter)
+{   
+    char * value;
+    while ((value = strtok(NULL, ",\n")) != NULL)
+    {
+        strcpy(parameter->array[parameter->count], value);
+        parameter->count++;
+    }
+}
+
+void readData()
+{
+    
 }
